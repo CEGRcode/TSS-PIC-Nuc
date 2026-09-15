1bdee71479edcd89a6c9e52b859f66b10fd94f1f
HeninChen <46940074+HeninChen@users.noreply.github.com>
2026-09-15 02:21:22 -0400

    Update directories and file copies for S11B and S13B

--- 0X_Bulk_Processing/11_GO-term-analysis_file_prepare.sh
+++ 0X_Bulk_Processing/11_GO-term-analysis_file_prepare.sh
@@ -1,10 +1,10 @@
 WRK=/Path/to/Title/
 ## determin output
 [ -d logs ] || mkdir logs
-[ -d $WRK/Library/E11 ] || mkdir -p $WRK/Library/E11
-[ -d $WRK/Library/E14 ] || mkdir -p $WRK/Library/E14
-cd $WRK/Library/E11
-cp $Reference/TSS_+1Nuc_*.bed $WRK/Library/E11
+[ -d $WRK/Library/S11B ] || mkdir -p $WRK/Library/S11B
+[ -d $WRK/Library/S13B ] || mkdir -p $WRK/Library/S13B
+cd $WRK/Library/S11B
+cp $Reference/TSS_+1Nuc_*.bed $WRK/Library/S11B
 
-cp $Reference/TSS_same_WDR5_M1.bed $WRK/Library/E14
-cp $Reference/TSS_all_phase_adj+1Nuc_Di.bed $WRK/Library/E14
+cp $Reference/TSS_same_WDR5_M1.bed $WRK/Library/S13B
+cp $Reference/TSS_all_phase_adj+1Nuc_Di.bed $WRK/Library/S13B
